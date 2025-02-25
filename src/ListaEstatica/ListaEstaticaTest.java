27303b1f513e2e366a3b130187f2dd5b64d6f545
Gabriel Eufrázio <geufrazio@furb.br>
2025-02-25 19:56:26 -0300

    listaEstatica

--- src/ListaEstatica/ListaEstaticaTest.java
+++ src/ListaEstatica/ListaEstaticaTest.java
@@ -1,4 +1,18 @@
 package ListaEstatica;
 
 public class ListaEstaticaTest {
+    ListaEstatica lista = new ListaEstatica();
+
+    public void testRetirarElemento() {
+        lista.inserir(5);
+        lista.inserir(10);
+        lista.inserir(15);
+        lista.inserir(20);
+
+        try {
+            lista.retirar(5);
+        } catch (IndexOutOfBoundsException e){
+
+        }
+    }
 }
